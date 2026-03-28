3041f44897c6b9bd78e5da1e158e90e9b3e14c84
phungdat24 <25021715@vnu.edu.vn>
2026-03-28 09:42:01 +0700

    Khởi tạo project Maven và javafx

---
+++ src/main/java/com/nhomX/example/Main.java
@@ -0,0 +1,22 @@
+package com.nhomX.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Layout.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),400,300 );
+        stage.setTitle("Auction Project");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.nhomX.example{
+    requires javafx.controls;
+    requires javafx.fxml;
+    opens com.nhomX.example to javafx.fxml;
+    exports com.nhomX.example;
+}
