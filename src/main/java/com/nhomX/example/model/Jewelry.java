caf9d2f83e0bf9bcbffec02c6456001a347d05e5
khanhduyah25021690-COMP8 <khanhduyah2503@gmail.com>
2026-05-02 00:10:35 +0700

    feat(ui): Them imagePath va hien thi anh cho 10 item

--- src/main/java/com/nhomX/example/model/Jewelry.java
+++ src/main/java/com/nhomX/example/model/Jewelry.java
@@ -1,11 +1,13 @@
 package com.nhomX.example.model;
 
-public class Jewelry extends Items{
+public class Jewelry extends Items {
     private String gemstoneType;
-    public Jewelry(String id, String title, String sellerId, String gemstoneType){
+
+    public Jewelry(String id, String title, String sellerId, String gemstoneType) {
         super(id, title, sellerId);
         this.gemstoneType = gemstoneType;
     }
+
     public void setGemstoneType(String gemstoneType) {
         this.gemstoneType = gemstoneType;
     }
