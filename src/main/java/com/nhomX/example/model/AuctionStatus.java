0f86084f4ec3903aac4b7b5973fc9c8ebfb51454
chiendominh21 <chiendominh21@gmail.com>
2026-05-25 16:05:49 +0700

    Thêm chức năng & fix

--- src/main/java/com/nhomX/example/model/AuctionStatus.java
+++ src/main/java/com/nhomX/example/model/AuctionStatus.java
@@ -3,6 +3,8 @@ package com.nhomX.example.model;
 public enum AuctionStatus {
     // Chờ ADMIN xét duyệt:
     PENDING,
+    // Da duoc Admin duyet nhung chua den gio bat dau
+    UP_COMING,
     // Phiên vừa được duyệt, chưa có ai đấu giá
     OPEN,
     // Đang diễn ra, có ít nhất 1 bid
--- src/main/java/com/nhomX/example/networking/ClientHandler.java
+++ src/main/java/com/nhomX/example/networking/ClientHandler.java
@@ -488,7 +488,12 @@ public class ClientHandler implements Runnable {
         }
 
         auction.setApprovedBy(currentUser.getId());
-        auction.setStatus(AuctionStatus.OPEN);
+        if (auction.getStartTime() != null
+                && LocalDateTime.now().isBefore(auction.getStartTime())) {
+            auction.setStatus(AuctionStatus.UP_COMING);
+        } else {
+            auction.setStatus(AuctionStatus.OPEN);
+        }
         boolean isSuccess = auctionRepository.updateAuctionStatus(auction);
         sendToClient(new Message("APPROVE_RESULT",
                 new Object[]{isSuccess, isSuccess ? "Đã duyệt sản phẩm." : "Duyệt thất bại!"}));
